5857a09c4f639bc6e04fab5256d2961a9bd7c2a9
Clebson1094 <clebson167.cj@gmail.com>
2025-07-15 14:23:17 -0300

    att

---
+++ teste.cs
@@ -0,0 +1,8 @@
+using System;
+public class printando
+{
+    public static void Main(string[] args)
+    {
+        console.WriteLine("Hello, World!");
+    }
+}
--- testes de codigos.py
+++ testes de codigos.py
@@ -1,20 +1,6 @@
-def deposito(carteira):
-    while True:
-        try:
-            deposito = float(input("Digite o valor de deposito"))
-            if deposito <= 0:
-                print("Não é aceito deposito zerado ou negativo")
-                continue
-            else:
-                print("Prestou")
-                carteira += deposito
-                return carteira
-        except ValueError:
-            print("Coloque o valor em números por favor!")
-            continue
+import os
+nome = input("Digite seu nome")
 
-saldo = 0
 
-saldo = deposito(saldo)
-
-print(saldo)
+with open("Ranking.txt", "a+") as f:
+    f.write(f"{nome}\n")
