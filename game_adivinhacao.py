017479402763c4d1464b257cc229d51270bfc81e
Clebson1094 <clebson167.cj@gmail.com>
2025-07-19 22:22:33 -0300

    tt

--- game_adivinhacao.py
+++ game_adivinhacao.py
@@ -4,19 +4,23 @@ from time import sleep
 print(" === BEM VINDO AO TIGRINHO LITE === ")
 sleep(1)
 saldo = 0
+vitorias = 0
+derrotas = 0
+nome = str(input("Digite seu nome: "))
 
 def codigos_promocionais(carteira):
     while True:
         codigo = str(input("Digite o seu codigo promocional, caso não tenha digite (JOGAR): ")).lower()
         match codigo:
             case "html css kkk" | "se a classe operária tudo produz, a ela tudo pertence" | "virginia" | "jailson utinguense":
-                return 50 + carteira
+                sleep(1)
+                print(f"Código aceito, foi adicionado R$:50,00 ao seu saldo para começar a jogar")
+                sleep(1)
+                return 50 + carteira  
             case "jogar":
                 return carteira + 0
             case _:
                 print("Codigo inexistente, caso não tenha digite (jogar)")
-
-saldo = codigos_promocionais(saldo)
 
 def niveis_de_apostas():
     while True:
@@ -37,7 +41,7 @@ def niveis_de_apostas():
 def deposito(carteira, restante):
     while True:
         try:
-            deposito = float(input(f"Seu saldo está R$:{carteira}, precisa de {restante} para iniciar o game: "))
+            deposito = float(input(f"Seu saldo está R$:{carteira:.2f}, precisa de {restante:.2f} para iniciar o game: "))
             if deposito <= 0:
                 print("Não é aceito deposito zerado ou negativo")
                 continue
@@ -53,20 +57,22 @@ def deposito(carteira, restante):
             print("Coloque o valor em números por favor!")
             continue
 
-def aposta(jogador, bot, saldo, ganho):
+def aposta(jogador, bot, saldo, ganho, victory, defeat):
     custos = {1: 9, 2: 15, 3: 20}
     lucros = {1: 0.09, 2: 0.15, 3: 0.20}
     custo = custos[ganho]
     lucro = lucros[ganho]
     if jogador == bot:
         saldo += saldo * lucro
+        victory += 1
         print(f"PARABENS!!! O número escolhido foi {bot}, já foi depositado em seu saldo seu lucro, ficando com R$:{saldo}")
     else:
         saldo -= custo
         print(f"Que pena o número escolhido foi {bot}, foi debitado {custo}")
         sleep(1)
         print("Vamos girar novamente")
-    return saldo
+        defeat += 1
+    return saldo, victory, defeat
 
 def numeros_possiveis_aposta():
     while True:
@@ -84,17 +90,21 @@ def numeros_possiveis_aposta():
         except ValueError:
             print("Apenas NÚMEROS de 1 a 10")
 
+saldo = codigos_promocionais(saldo)
+
 while True:
     try:
-        numero_bot = randint(1, 10)
         if saldo < 50:
             diferença = 50 - saldo
             saldo = deposito(saldo, diferença)
         else:
+            numero_bot = randint(1, 10)
             nivel = niveis_de_apostas()
-            print(f"Você tem {saldo} de saldo, vamos jogar")
+            print(f"Você tem {saldo:.2f} de saldo, vamos jogar")
             usuario = numeros_possiveis_aposta()
-            saldo = aposta(usuario, numero_bot, saldo, nivel)
+            saldo, vitorias, derrotas = aposta(usuario, numero_bot, saldo, nivel, vitorias, derrotas)
+            sleep(1.5)
+            print(f"Placar\n{nome}: ({vitorias}) | Tigrinho: ({derrotas})")
             sleep(1.5)
     except ValueError:
         continuar = input("Não consegui compreender o que disse, deseja continuar? (s/n) ")
--- testes de codigos.py
+++ testes de codigos.py
@@ -1,20 +1,15 @@
-from time import sleep
-def numeros_possiveis_aposta(jogador):
-    while True:
-        try:
-            jogador = int(input("Digite um número inteiro de 1 a 10: "))
-            if 1 <= jogador <= 10:
-                print("Sorteando...")
-                sleep(1)
-                for i in range(1, 4):
-                    print(f"{i}...")
-                    sleep(1)
-                return jogador
-            else:
-                print("Tem quer ser de 1 a 10")
-                sleep(1)
-        except ValueError:
-            print("Apenas NÚMEROS de 1 a 10")
-            sleep(1)
+vitorias = 0
+derrotas = 0
 
-numeros_possiveis_aposta()
+bot = 2
+
+while True:
+    humano = int(input("Digite"))
+    if humano == bot:
+        vitorias += 1
+    else:
+        derrotas += 1
+        humano = False
+
+    print(vitorias, derrotas)
+    print(bool(humano))
